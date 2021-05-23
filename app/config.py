ec0f0c07d6666f8fa76fd98f84e00322442d6f6a
Sidharth <sidharthajithkumar@gmail.com>
2021-05-23 23:42:50 +0530

    removed .idea

--- app/config.py
+++ app/config.py
@@ -9,14 +9,9 @@ load_dotenv()
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
-GUILD_ID = os.environ.get("GUILD_ID")
-
 DEBUG = os.environ.get("DEBUG", "false") != "false"
 
 if DEBUG:
     logging.basicConfig(level=logging.DEBUG)
 else:
     logging.basicConfig(level=logging.INFO)
-
-if not GUILD_ID and DEBUG:
-    logging.warning("GUILD_ID shouldn't be empty in debug mode")
