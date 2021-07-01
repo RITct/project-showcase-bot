7db33df2ac860bee84c43ac2c209fa8da8a036bf
abhinavk001 <abhinavk5544@gmail.com>
2021-07-01 21:31:19 +0530

    website

--- public/static/script.js
+++ public/static/script.js
@@ -1,1 +1,11 @@
-// Add your scripts here
+function navDropdown() {
+  var x = document.getElementById("nav");
+  var home = document.getElementById("home");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+    home.className += " blur";
+  } else {
+    x.className = "navbar";
+    home.className = "home";
+  }
+}
